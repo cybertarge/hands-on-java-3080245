903c6c949cf047b83e9cdf09895dcad1847ab03e
cybertarge <billy@cybertarge.com>
2023-09-05 19:34:38 +0000

    s

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -2,6 +2,7 @@ package bank;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DataSource {
@@ -19,6 +20,19 @@ public class DataSource {
 
     return connection;
   }
+
+  public static Customer getCustomer(String username) {
+    String sql = "select * from customers where usename = ?";
+    try(Connection connection = connect();
+      PreparedStatement statement = connection.prepareStatement(sql)){
+
+    }catch(SQLException e){
+      e.printStackTrace();
+    }
+  }
+
+
+
 
   public static void main(String[] args){
     connect();
