87feefed0bf27012a5556f1a8cd166c83254efaa
cybertarge <billy@cybertarge.com>
2023-09-05 20:18:05 +0000

    Let's save this now

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -3,7 +3,10 @@ package bank;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.xml.transform.Result;
 
 public class DataSource {
   
@@ -23,18 +26,30 @@ public class DataSource {
 
   public static Customer getCustomer(String username) {
     String sql = "select * from customers where usename = ?";
+    Customer customer = null;
     try(Connection connection = connect();
       PreparedStatement statement = connection.prepareStatement(sql)){
 
+        statement.setString(1, username);
+        try(ResultSet resultSet = statement.executeQuery()){
+          customer = new Customer(
+            resultSet.getInt(columnLabel: "id"),
+            resultSet.getString(columnLabel: "name"),
+            resultSet.getString( "username"),
+            resultSet.getString( "password"),
+            resultSet.getInt(columnLabel: "account_id"));
+        }
     }catch(SQLException e){
       e.printStackTrace();
     }
+    return customer;
   }
 
 
 
 
   public static void main(String[] args){
-    connect();
+    Customer customer = getCustomer("twest8o@friendfeed.com");
+    System.out.println(customer.getName());
   }
 }
